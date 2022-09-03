e677d9d1f0556ee6efc84a033c0ddfc6a6e06471
Minh Tin Tang <minhtintang@gmail.com>
2022-09-03 10:37:38 +0700

    02-data-models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Post, Author, Tag
+
+
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'date', 'excerpt')
+    list_filter = ('author', 'tag', 'date')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
